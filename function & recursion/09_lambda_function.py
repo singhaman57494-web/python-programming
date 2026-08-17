24176f33ede57e267a3182a3b35f398d8730487f
singhaman57494-web <singhaman57494@gmail.com>
2026-08-17 23:54:12 +0530

    added my code

--- function & recursion/09_lambda_function.py
+++ function & recursion/09_lambda_function.py
@@ -1,7 +1,17 @@
 #                                  lambda function
+'''
+ A function is a small, anonymous function in python. it is useful when you need 
+ a simple function for a short operation.
 
-#syntax:-  lambda arguments: expression
+syntax:-  lambda arguments: expression '''
 
 add = lambda a, b: a + b
 
 print(add(5, 3))
+
+
+#   square
+
+square = lambda x: x *x
+
+print(square(5))
