84a48b0ee28285915d9c406ea99feb3ac4ee5834
singhaman57494-web <singhaman57494@gmail.com>
2026-09-07 23:53:15 +0530

    Added

--- Data structure and Algorithm(DSA)/queue/priority_queue.py
+++ Data structure and Algorithm(DSA)/queue/priority_queue.py
@@ -1,3 +1,11 @@
 import heapq
 
 hospital = []
+
+heapq.heappush(hospital, (3, "broken finger"))
+heapq.heappush(hospital, (1, "Hart atteck"))
+heapq.heappush(hospital, (2, "hand broken"))
+
+print(heapq.heappop(hospital))
+print(heapq.heappop(hospital))
+print(heapq.heappop(hospital))
