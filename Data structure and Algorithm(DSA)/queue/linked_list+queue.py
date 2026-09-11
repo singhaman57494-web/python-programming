c49e50e17623a4aee44271c1c54d639c02a83179
singhaman57494-web <singhaman57494@gmail.com>
2026-09-12 00:47:30 +0530

    added this code

--- Data structure and Algorithm(DSA)/queue/linked_list+queue.py
+++ Data structure and Algorithm(DSA)/queue/linked_list+queue.py
@@ -20,11 +20,34 @@ class queue:
 
         return removed
 
+    def enqueue(self, data):
+        node6 = node(data)
+
+        if self.front == None:
+            self.front = node6
+            self.rear = node6
+        else:
+            self.rear.next = node6
+            self.rear = node6
+            
+    def peek(self):
+        if self.front == None:
+            return "empty queue"
+        return self.front.data
+
+    def is_empty(self):
+        if self.front == None:
+            return True
+        return False
+
+        
+
 node1 = node(10)
 node2 = node(20)
 node3 = node(30)
 node4 = node(40)
 node5 = node(50)
+
 
 node1.next = node2
 node2.next = node3
@@ -39,3 +62,9 @@ print("front : ", q.front.data)
 print("rear :", q.rear.data)
 print("dequeue :", q.dequeue())
 print("front : ", q.front.data)
+
+print("peek : ", q.peek())
+
+print("queue is empty : ", q.is_empty())
+
+print("new node : ", q.rear.data)
--- Data structure and Algorithm(DSA)/queue/tempCodeRunnerFile.py
+++ Data structure and Algorithm(DSA)/queue/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-def dequeue(self):
-        if self.front == None:
-            return "queue is empty"
-
-        return self.front = self.front.next
+q.pe
